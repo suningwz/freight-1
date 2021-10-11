74bec8a51115b016a42a2aac1dd4733131bc7bdc
Muhammad Zulfi Rusdani <donimuzur@gmail.com>
2021-10-11 09:33:07 +0700

    fix report double input

--- models/wizard_print_rekap_shift.py
+++ models/wizard_print_rekap_shift.py
@@ -54,19 +54,19 @@ class DORekapPrint(models.AbstractModel):
         docs = []
         self.env.cr.execute(
             """
-            select a.gardu, a.shift, d.name as kendaraan, c.name as produk, count(a.do_id) as qty
+             select a.gardu, a.shift, d.name as kendaraan, c.name as produk, count(a.do_id) as qty
                 from 
-	                freight_attendance a
+	                ( select tipe_kendaraan, produk, gardu, shift, do_id, tanggal from freight_attendance where tanggal = '%s' group by gardu, shift,produk,tipe_kendaraan,do_id, tanggal) as a
                 left join 
                     (product_product b left join product_template c on b.product_tmpl_id=c.id ) on a.produk = b.id
                 left join
                     tipe_kendaraan d on a.tipe_kendaraan = d.id
                 left join
                     delivery_order e on a.do_id = e.id
-                where a.tanggal = '%s' and gardu = %s and shift = %s and (e.state='open' or e.state='done') 
-                group by a.gardu, a.shift, d.name, c.name
+                where a.tanggal = '%s' and gardu =%s and shift =  %s and (e.state='open' or e.state='done') 
+		group by a.gardu, a.shift, d.name, c.name
                 order by a.gardu, a.shift, d.name, c.name
-            """ % (date_start, pos_id, shift_id))
+            """ % (date_start, date_start, pos_id, shift_id))
 
         hasil = self.env.cr.fetchall()
         for baris in hasil:
